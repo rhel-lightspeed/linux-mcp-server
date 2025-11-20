92ffb6b4d8b3711723b2d89c797c1fdef79d0a6e
Sam Doran <sdoran@redhat.com>
2025-11-20 17:48:06 -0500

    Add configuration

---
+++ src/linux_mcp_server/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="LINUX_MCP_", env_ignore_empty=True)
+
+    user: str = ""
+
+
+CONFIG = Config()
