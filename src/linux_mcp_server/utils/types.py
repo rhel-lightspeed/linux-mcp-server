05bee18be80bbf27c8198cde0b19f5b6ac6b6a21
Sam Doran <sdoran@redhat.com>
2025-11-22 15:46:09 -0500

    Remove Username type hint since it is no longer used

--- src/linux_mcp_server/utils/types.py
+++ src/linux_mcp_server/utils/types.py
@@ -4,4 +4,3 @@ from pydantic import Field
 
 
 Host = t.Annotated[str, Field(description="Remote host to connect to")]
-Username = t.Annotated[str, Field(description="SSH username (if not provided, the current user account is used)")]
