ca34cf50b2658d3abd414926b22989e7c04ebb55
Major Hayden <major@mhtx.net>
2025-12-10 10:22:40 -0600

    Filter out IPv6 link-local addresses (#75)
    IPv6 link-local addresses are automatically assigned but they are poor
    options for reliable connectivity in an IPv6 network. Filter out these
    addresses carefully.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- src/linux_mcp_server/utils/__init__.py
+++ src/linux_mcp_server/utils/__init__.py
@@ -1,3 +1,3 @@
 from linux_mcp_server.utils.enum import StrEnum as StrEnum
 from linux_mcp_server.utils.format import format_bytes as format_bytes
-from linux_mcp_server.utils.format import is_ipv6_link_local
+from linux_mcp_server.utils.format import is_ipv6_link_local as is_ipv6_link_local
