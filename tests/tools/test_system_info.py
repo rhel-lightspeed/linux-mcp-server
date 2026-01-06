d997ff44588281961126103e216bd2d2185862f6
Sam Doran <sdoran@redhat.com>
2026-01-06 17:49:19 -0500

    Increase test coverage some more

--- tests/tools/test_system_info.py
+++ tests/tools/test_system_info.py
@@ -5,6 +5,11 @@ import sys
 import pytest
 
 
+@pytest.fixture
+def mock_execute(mock_execute_with_fallback_for):
+    return mock_execute_with_fallback_for("linux_mcp_server.commands")
+
+
 @pytest.mark.skipif(sys.platform != "linux", reason="requires Linux commands")
 @pytest.mark.parametrize(
     "tool, expected",
@@ -47,3 +52,35 @@ async def test_system_info_tools(tool, expected, mcp_client):
     result_text = result.content[0].text.casefold()
 
     assert all(any(n in result_text for n in case) for case in expected), "Did not find all expected values"
+
+
+@pytest.mark.parametrize(
+    "tool",
+    (
+        "get_memory_information",
+        "get_disk_usage",
+    ),
+)
+async def test_system_info_tools_unsuccessful(tool, mcp_client, mock_execute):
+    mock_execute.return_value = (0, "", "")
+
+    result = await mcp_client.call_tool(tool)
+    result_text = result.content[0].text.casefold()
+
+    assert "error" in result_text
+
+
+@pytest.mark.parametrize(
+    "tool",
+    (
+        "get_system_information",
+        "get_cpu_information",
+    ),
+)
+async def test_system_info_tools_unsuccessful_empty(tool, mcp_client, mock_execute):
+    mock_execute.return_value = (0, "", "")
+
+    result = await mcp_client.call_tool(tool)
+    result_text = result.content[0].text.casefold()
+
+    assert result_text == ""
