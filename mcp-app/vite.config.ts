ae3ab505330e83b8e4b64242d4bc80024c8f997d
Chih-Tao Lee <jason101011113@gmail.com>
2026-04-14 10:33:41 -0400

    chore: update vite and vitejs/plugin-react (#392)
    Got this warning: "WARN  inlineDynamicImports option is deprecated, please use codeSplitting:
    false instead", which is because vite-plugin-singlefile has not adapted to this new naming
    of output option in Vite 8. https://github.com/richardtallent/vite-plugin-singlefile/pull/119
    seems to fix the warning.
    
    Overall, it's just a warning which doesn't cause a problem. The app is successfully built
    and run on both Goose desktop and Claude desktop without any issues.
    
    Note: Passing inlineDynamicImports actually makes `npm run build:prod` fail in the UBI 10
    container with a segfualt error. I tested with Node image and it worked fine, so I guess the
    segfault was due to UBI strips off some libraries that was needed in the cases of passing
    deprecated option to Rolldown. The fix here is to opt out the recommended options from
    vite-plugin-singlefile and provide them ourselves in vite.config.ts. Also, the PR to
    vite-plugin-singlefile I mentioned above also fixes the segfault bug by providing the
    up-to-date options, but it has not been merged yet. We can opt back in with the recommended
    options when there is a new version that contains this fix.

--- mcp-app/vite.config.ts
+++ mcp-app/vite.config.ts
@@ -10,11 +10,21 @@ if (!INPUT) {
 const isDevelopment = process.env.NODE_ENV === "development";
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [
+    react(),
+    // Disable useRecommendedBuildConfig because it sets inlineDynamicImports,
+    // which causes a segfault in the container using ubi10 image
+    viteSingleFile({ useRecommendedBuildConfig: false }),
+  ],
+  base: "./",
   build: {
     sourcemap: isDevelopment ? "inline" : undefined,
     cssMinify: !isDevelopment,
     minify: !isDevelopment,
+    assetsInlineLimit: () => true,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    assetsDir: "",
 
     rollupOptions: {
       input: INPUT,
