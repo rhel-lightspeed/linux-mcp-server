19ddba4ac57c4f2db0f03a14b43a49b929e4c826
Jazzcort <jason101011113@gmail.com>
2026-07-01 16:36:12 -0400

    Add get_execution_timeout to RUN_SCRIPT_APP_TOOLS for the list tool test

--- tests/test_server.py
+++ tests/test_server.py
@@ -49,6 +49,7 @@ RUN_SCRIPT_TOOLS = set(["run_script", "run_script_with_confirmation", "validate_
 RUN_SCRIPT_APP_TOOLS = set(
     [
         "execute_script",
+        "get_execution_timeout",
         "get_execution_state",
         "reject_script",
         "run_script_interactive",
